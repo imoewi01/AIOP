d3fadb6978612758e0ad57d05d92b6127391062e
William Imoehl <williamimoehl@gmail.com>
2025-07-03 15:01:37 -0600

    Add files via upload
    Current scripts and notebooks for the AIOP project

--- cleanAndConvertData_txt_to_csv.py
+++ cleanAndConvertData_txt_to_csv.py
@@ -1,7 +1,7 @@
 
 
-def clean_file(inputFilePath, ColumnName, outputFilePath, badValues=["<<","Network","disconnection",">>"]):
-    
+def clean_file(inputFilePath, ColumnName, outputFilePath, badValues=["<<","Network","disconnection",">>","<"]):
+
     print("starting file",inputFilePath)
     # need to replace things like
     # << Network disconnection >>,
@@ -26,16 +26,17 @@ def clean_file(inputFilePath, ColumnName, outputFilePath, badValues=["<<","Netwo
             if split_line[2] in badValues:
                 print(split_line)
                 split_line[2] = "Undefined"
+
                 continue # for now just skip if it is undefined or 0
 
             output_time = split_line[0]+"T"+split_line[1]
             # convert split_line[0] and split_line[1] to a python datetime friendly format for later
-            output_line = output_time+","+split_line[2]
-
-            # some readback variables have multiple readings for each second 
+            #output_line = output_time+","+split_line[2]# this breaks radiator name, which has spaces in the actual variable values, using [2:] should work
+            output_line = output_time+","+"".join(split_line[2:])
+            # some readback variables have multiple readings for each second
             # don't have time precision to keep all readings, so just take first in those cases
             if output_time==last_time:
-                continue 
+                continue
             else:
                 last_time = output_time
 
@@ -56,35 +57,36 @@ def clean_file(inputFilePath, ColumnName, outputFilePath, badValues=["<<","Netwo
 # these will later be merged into a single csv
 if __name__ == "__main__":
 
+    run_periods = ["Spring2020","Spring2023","Spring2025"]
 
-    # all run configuration info 
-    clean_file("rawData/runConfigFiles_Spring2020/cbrem_plane.txt","CBREM:PLANE","csv_data/initial_converted/cbrem_plane.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/daq_status.txt","DAQ:STATUS","csv_data/initial_converted/daq_status.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_pitch_rbv.txt","GONI:PITCH.RBV","csv_data/initial_converted/goni_pitch_rbv.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_pitch.txt","GONI:PITCH","csv_data/initial_converted/goni_pitch.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_roll_rbv.txt","GONI:ROLL.RBV","csv_data/initial_converted/goni_roll_rbv.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_roll.txt","GONI:ROLL","csv_data/initial_converted/goni_roll.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_x_rbv.txt","GONI:X.RBV","csv_data/initial_converted/goni_x_rbv.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_x.txt","GONI:X","csv_data/initial_converted/goni_x.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_y_rbv.txt","GONI:Y.RBV","csv_data/initial_converted/goni_y_rbv.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_y.txt","GONI:Y","csv_data/initial_converted/goni_y.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_yaw_rbv.txt","GONI:YAW.RBV","csv_data/initial_converted/goni_yaw_rbv.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/goni_yaw.txt","GONI:YAW","csv_data/initial_converted/goni_yaw.csv")
-    clean_file("rawData/runConfigFiles_Spring2020/run_number.txt","RunNumber","csv_data/initial_converted/run_number.csv",["<<","0"])
+    for runPeriod in run_periods:
+        # all run configuration info
+        clean_file("rawData/"+runPeriod+"/cbrem_plane.txt","CBREM:PLANE","csv_data/initial_converted/"+runPeriod+"/cbrem_plane.csv")
+        clean_file("rawData/"+runPeriod+"/daq_status.txt","DAQ:STATUS","csv_data/initial_converted/"+runPeriod+"/daq_status.csv")
+        clean_file("rawData/"+runPeriod+"/goni_pitch_rbv.txt","GONI:PITCH.RBV","csv_data/initial_converted/"+runPeriod+"/goni_pitch_rbv.csv")
+        clean_file("rawData/"+runPeriod+"/goni_pitch.txt","GONI:PITCH","csv_data/initial_converted/"+runPeriod+"/goni_pitch.csv")
+        clean_file("rawData/"+runPeriod+"/goni_roll_rbv.txt","GONI:ROLL.RBV","csv_data/initial_converted/"+runPeriod+"/goni_roll_rbv.csv")
+        clean_file("rawData/"+runPeriod+"/goni_roll.txt","GONI:ROLL","csv_data/initial_converted/"+runPeriod+"/goni_roll.csv")
+        clean_file("rawData/"+runPeriod+"/goni_x_rbv.txt","GONI:X.RBV","csv_data/initial_converted/"+runPeriod+"/goni_x_rbv.csv")
+        clean_file("rawData/"+runPeriod+"/goni_x.txt","GONI:X","csv_data/initial_converted/"+runPeriod+"/goni_x.csv")
+        clean_file("rawData/"+runPeriod+"/goni_y_rbv.txt","GONI:Y.RBV","csv_data/initial_converted/"+runPeriod+"/goni_y_rbv.csv")
+        clean_file("rawData/"+runPeriod+"/goni_y.txt","GONI:Y","csv_data/initial_converted/"+runPeriod+"/goni_y.csv")
+        clean_file("rawData/"+runPeriod+"/goni_yaw_rbv.txt","GONI:YAW.RBV","csv_data/initial_converted/"+runPeriod+"/goni_yaw_rbv.csv")
+        clean_file("rawData/"+runPeriod+"/goni_yaw.txt","GONI:YAW","csv_data/initial_converted/"+runPeriod+"/goni_yaw.csv")
+        clean_file("rawData/"+runPeriod+"/run_number.txt","RunNumber","csv_data/initial_converted/"+runPeriod+"/run_number.csv",["<<","0"])
 
 
-    # photon and electron beam info
-    clean_file("rawData/beamVariables/ac_x.txt","AC:X","csv_data/initial_converted/ac_x.csv")
-    clean_file("rawData/beamVariables/ac_y.txt","AC:Y","csv_data/initial_converted/ac_y.csv")
-    clean_file("rawData/beamVariables/ebeam_current.txt","EBEAM:CURRENT","csv_data/initial_converted/ebeam_current.csv")
-    clean_file("rawData/beamVariables/ebeam_energy.txt","EBEAM:ENERGY","csv_data/initial_converted/ebeam_energy.csv")
-    clean_file("rawData/beamVariables/ebeam_x.txt","EBEAM:X","csv_data/initial_converted/ebeam_x.csv")
-    clean_file("rawData/beamVariables/ebeam_y.txt","EBEAM:Y","csv_data/initial_converted/ebeam_y.csv")
-    clean_file("rawData/beamVariables/photonbeam_energy_desired.txt","PBEAM:SET","csv_data/initial_converted/photonbeam_energy_desired.csv")
-    clean_file("rawData/beamVariables/photonbeam_energy_uncertainty.txt","PBEAM:WIDTH","csv_data/initial_converted/photonbeam_energy_uncertainty.csv")
-    clean_file("rawData/beamVariables/photonbeam_energy.txt","PBEAM:ENERGY","csv_data/initial_converted/photonbeam_energy.csv")
-    clean_file("rawData/beamVariables/radiator_id.txt","RADIATOR:ID","csv_data/initial_converted/radiator_id.csv")
-    clean_file("rawData/beamVariables/radiator_name.txt","RADIATOR:NAME","csv_data/initial_converted/radiator_name.csv")
-    
-    
-
+        # photon and electron beam info
+        clean_file("rawData/"+runPeriod+"/ac_x.txt","AC:X","csv_data/initial_converted/"+runPeriod+"/ac_x.csv")
+        clean_file("rawData/"+runPeriod+"/ac_y.txt","AC:Y","csv_data/initial_converted/"+runPeriod+"/ac_y.csv")
+        clean_file("rawData/"+runPeriod+"/ebeam_current.txt","EBEAM:CURRENT","csv_data/initial_converted/"+runPeriod+"/ebeam_current.csv")
+        clean_file("rawData/"+runPeriod+"/ebeam_energy.txt","EBEAM:ENERGY","csv_data/initial_converted/"+runPeriod+"/ebeam_energy.csv")
+        clean_file("rawData/"+runPeriod+"/ebeam_x.txt","EBEAM:X","csv_data/initial_converted/"+runPeriod+"/ebeam_x.csv")
+        clean_file("rawData/"+runPeriod+"/ebeam_y.txt","EBEAM:Y","csv_data/initial_converted/"+runPeriod+"/ebeam_y.csv")
+        clean_file("rawData/"+runPeriod+"/photonbeam_energy_desired.txt","PBEAM:SET","csv_data/initial_converted/"+runPeriod+"/photonbeam_energy_desired.csv")
+        clean_file("rawData/"+runPeriod+"/photonbeam_energy_uncertainty.txt","PBEAM:WIDTH","csv_data/initial_converted/"+runPeriod+"/photonbeam_energy_uncertainty.csv")
+        clean_file("rawData/"+runPeriod+"/photonbeam_energy.txt","PBEAM:ENERGY","csv_data/initial_converted/"+runPeriod+"/photonbeam_energy.csv")
+        clean_file("rawData/"+runPeriod+"/radiator_id.txt","RADIATOR:ID","csv_data/initial_converted/"+runPeriod+"/radiator_id.csv")
+        clean_file("rawData/"+runPeriod+"/radiator_name.txt","RADIATOR:NAME","csv_data/initial_converted/"+runPeriod+"/radiator_name.csv")
+        if runPeriod=='Spring2023' or runPeriod=='Spring2025':
+            clean_file("rawData/"+runPeriod+"/beam_lock.txt","AC:X","csv_data/initial_converted/"+runPeriod+"/beam_lock.csv")
